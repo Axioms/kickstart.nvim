89e206e2e50e19b64dc557272a9230b5ed8f0f5a
Axioms <atomic.axioms@gmail.com>
2023-03-05 19:50:36 -0500

    first changes

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,16 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+	-- Command Completion
+	'smolck/command-completion.nvim',
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v2.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+		},
+  }
+}
