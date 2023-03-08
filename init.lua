acf851aa22394100e269dc8b3d65c19cd77b9352
Axioms <atomic.axioms@gmail.com>
2023-03-08 15:08:04 -0500

    fixed lazy being removed

--- init.lua
+++ init.lua
@@ -50,7 +50,7 @@ if not vim.loop.fs_stat(lazypath) then
     'git',
     'clone',
     '--filter=blob:none',
-    '.git',
+    'https://github.com/folke/lazy.nvim.git',
     '--branch=stable', -- latest stable release
     lazypath,
   }
